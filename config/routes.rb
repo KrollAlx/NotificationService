fb16ba74eadbf4f75cd86b654e5c6a4ad44e6b63
KrollAlx <Aleksiy2000@yandex.ru>
2022-12-27 20:18:24 +0300

    swagger and Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/docs'
-  mount Rswag::Api::Engine => '/docs'
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
 
   post '/clients', to: 'clients#create'
   put '/clients/:id', to: 'clients#update'
